d779eb79f2377304eb30d3e455cdbb595b74e8bc
vincenterc <chung.menghsun@gmail.com>
2019-11-05 00:15:44 +0800

    Add some variables to Player class

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -7,6 +7,16 @@ public class Player : MonoBehaviour {
 	public Vector3 moveDestination;
 	public float moveSpeed = 10.0f;
 
+	public bool moving = false;
+	public bool attacking = false;
+
+	public int HP = 25;
+
+	public float attackChance = 0.75f;
+	public float defenseReduction = 0.15f;
+	public int damageBase = 5;
+	public float damageRollSides = 6; // d6
+
 	void Awake() {
 		moveDestination = transform.position;
 	}
