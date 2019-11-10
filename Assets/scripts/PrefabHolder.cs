94c666b194c55354e133d32831099b7cab529464
vincenterc <chung.menghsun@gmail.com>
2019-11-10 20:33:17 +0800

    Add  PrefabHolder script

---
+++ Assets/scripts/PrefabHolder.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabHolder : MonoBehaviour {
+	public static PrefabHolder instance;
+
+	public GameObject BASE_TILE_PREFAB;
+
+	public GameObject TILE_NORMAL_PREFEB;
+	public GameObject TILE_DIFFICULT_PREFEB;
+	public GameObject TILE_VERY_DIFFICULT_PREFEB;
+	public GameObject TILE_IMPASSIBLE_PREFEB;
+
+	void Awake() {
+		instance = this;
+	}
+}
