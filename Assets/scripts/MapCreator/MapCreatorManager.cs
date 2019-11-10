2ff6f75529b99332562a7fc66df86841326ab659
vincenterc <chung.menghsun@gmail.com>
2019-11-10 20:33:57 +0800

    Add map related scripts

---
+++ Assets/scripts/MapCreator/MapCreatorManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapCreatorManager : MonoBehaviour {
+
+	public int mapSize;
+	public List<List<Tile>> map = new List<List<Tile>>();
+
+	void Awake() {
+		generateBlankMap(22);
+	}
+
+	// Update is called once per frame
+	void Update() {
+
+	}
+
+	void generateBlankMap(int mSize) {
+		mapSize = mSize;
+
+		map = new List<List<Tile>>();
+		for (int i = 0; i < mapSize; i++) {
+			List<Tile> row = new List<Tile>();
+			for (int j = 0; j < mapSize; j++) {
+				Tile tile = ((GameObject) Instantiate(PrefabHolder.instance.BASE_TILE_PREFAB, new Vector3(i - Mathf.Floor(mapSize / 2), 0, -j + Mathf.Floor(mapSize / 2)), Quaternion.Euler(new Vector3()))).GetComponent<Tile>();
+				tile.gridPosition = new Vector2(i, j);
+				tile.setType(TileType.Normal);
+				row.Add(tile);
+			}
+			map.Add(row);
+		}
+	}
+}
---
+++ Assets/scripts/MapCreator/MapSaveLoad.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class TileXml {
+	[XmlAttribute("id")]
+	public int id;
+
+	[XmlAttribute("locX")]
+	public int locX;
+
+	[XmlAttribute("locY")]
+	public int locY;
+}
+
+[XmlRoot("MapCollection")]
+public class MapXmlContainer {
+	[XmlArray("Tiles")]
+	[XmlArrayItem("Tile")]
+	public List<TileXml> tiles = new List<TileXml>();
+}
+
+public static class MapSaveLoad {
+	public static MapXmlContainer CreateMapContainer(List<List<Tile>> map) {
+
+		List<TileXml> tiles = new List<TileXml>();
+
+		for (int i = 0; i < map.Count; i++) {
+			for (int j = 0; j < map[i].Count; j++) {
+				tiles.Add(MapSaveLoad.CreateTileXml(map[i][j]));
+			}
+		}
+
+		return new MapXmlContainer() {
+			tiles = tiles
+		};
+	}
+
+	public static TileXml CreateTileXml(Tile tile) {
+		return new TileXml() {
+			id = (int) tile.type,
+				locX = (int) tile.gridPosition.x,
+				locY = (int) tile.gridPosition.y
+		};
+	}
+
+	public static void Save(MapXmlContainer mapContainer, string filename) {
+		var serializer = new XmlSerializer(typeof(MapXmlContainer));
+		using(var stream = new FileStream(filename, FileMode.Create)) {
+			serializer.Serialize(stream, mapContainer);
+		}
+	}
+
+	public static MapXmlContainer Load(string filename) {
+		var serializer = new XmlSerializer(typeof(MapXmlContainer));
+		using(var stream = new FileStream(filename, FileMode.Open)) {
+			return serializer.Deserialize(stream) as MapXmlContainer;
+		}
+	}
+}
